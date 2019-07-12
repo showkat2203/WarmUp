6c05df8462afafaa3c17c0e27c4ea737c495183e
showkat2203 <showkat2203@diu.edu.bd>
2019-07-12 18:01:07 +0600

    access private member without friend function

---
+++ access_private_member_without_friend_func.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+
+using namespace std ;
+
+
+class Test
+{
+private:
+    int data;
+public:
+    Test() { data = 0; }
+    int getData() { return data; }
+};
+
+int main()
+{
+    Test t;
+    int* ptr = (int*)&t;
+    *ptr = 10;
+    cout << t.getData();
+    return 0;
+}
